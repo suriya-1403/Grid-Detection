77955f5ffa5b3932f437616b82423f75622b66d9
suriyakrishnan <suriya@outlook.in>
2020-08-31 15:43:02 +0530

    Linked new Image

--- detect.py
+++ detect.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('sudoku 2.jpeg')
+img = cv2.imread('Sudoku.png')
 #current_image = cv2.resize(img, (32, 32))
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(gray, 50, 150, apertureSize=3)
